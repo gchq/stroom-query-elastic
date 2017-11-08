155d88b17dd1fa35930e77a2f9ad6b3b3c04afd4
GCHQ-11 <gchq.developer.11@gmail.com>
2017-11-08 09:48:51 +0000

    erm, putting back all the project files that got wiped in a git-fu mess up

---
+++ travis.script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#exit script on any error
+set -e
+
+#TODO: Update this build file to support CRON jobs.
+./gradlew -Pversion=$TRAVIS_TAG clean build shadowJar downloadUrlDependencies
+
+if [ -n "$TRAVIS_TAG" ]; then
+    echo "Travis Tag is Set, Sending to Docker"
+
+    # Conditionally tag git and build docker image.
+    # The username and password are configured in the travis gui
+    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+
+    ELASTIC_TAG="gchq/stroom-query-elastic:${TRAVIS_TAG}"
+    echo "Building stroom-query-elastic with tag ${ELASTIC_TAG}"
+    docker build --tag=${ELASTIC_TAG} .
+    echo "Pushing ${ELASTIC_TAG}"
+    docker push ${ELASTIC_TAG}
+else
+    echo "Skipping Docker Push - No Tag Set"
+fi
